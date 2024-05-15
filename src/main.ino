41fb4699fa47c382a9901412b9985a30073dd996
Hugo Hennetin <hugo.hennetin@proton.me>
2024-05-15 08:59:38 +0200

    base

---
+++ src/main.ino
@@ -0,0 +1,28 @@
+const int led1 = 2;
+const int led2 = 3;
+const int button = 4;
+int buttonState;
+int lastLed = led1;
+
+void setup() {
+    pinMode(led1, OUTPUT);
+    pinMode(led2, OUTPUT);
+    pinMode(button, INPUT);
+
+    digitalWrite(led1, HIGH);
+}
+
+void loop() {
+    buttonState = digitalRead(button);
+    if (buttonState != HIGH) {
+        if (lastLed == led1) {
+            digitalWrite(led1, LOW);
+            digitalWrite(led2, HIGH);
+            lastLed = led2;
+        } else {
+            digitalWrite(led2, LOW);
+            digitalWrite(led1, HIGH);
+            lastLed = led1;
+        }
+    }
+}
